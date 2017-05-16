4185fd5c7d75f419410322181972c846243da173
Noah Walker <noah@nwalker.me>
2017-05-16 15:02:05 -0600

    version 1.1 - filelist support

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Boiler - Multi-language Code Preprocessor &amp; Module Loader
 
 Have some code that's split into modules, but you don't want to copy-paste, deal with config, etc.?
-Boiler to the rescue! Currently at version 0.0.1 (Early WIP).
+Boiler to the rescue! Currently at version 1.1. It even supports non-programming languages (ex. English). It's as easy as:
 ```
-boil <module-name> [as <variable>]
+boil <module-name>
 ```
+
+## Modules
+Modules aren't anything that special. ```boil <module-name>``` just loads whatever the name is + .boil and puts it in the 
+.boiled file. It checks relative paths, then the home path (~/.boiler/).
+
+## Using the tool
+Run ```boiler <filename>``` to boil a file or ```boiler``` to run a file list as specified in boiler.files.txt.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //! # Boiler - Multi-language Code Preprocessor & Module Loader
 //! It's like webpack for everything! It grabs a file with the name
 //! specified in whatever is being boiled's 'recipe' section.
+//! Checks the same folder for a .boil, then the global folder for a .boil.
 //! Copyright (c) 2017 Noah Walker
 
 
@@ -9,26 +10,45 @@ use std::env;
 use std::fs::File;
 use std::io::{Read, Write};
 
+/// Returns name with .boiled before actual extension
+/// # Examples
+/// ```assert_eq!('example.boiled.secret.txt', boiled)```
+fn get_boiled_name(filepath: &str) -> String {
+    let (fp1, fp2) = filepath.split_at(filepath.find('.').unwrap());
+    fp1.to_owned() + ".boiled" + fp2
+}
+
 /// 'Shop' for 'Ingredients' - Get the replacements through known paths.
 fn shop(filepath: &str) -> Result<File, io::Error> {
     match File::open(filepath.to_owned() + ".boil") {
         Ok(e) => Ok(e),
         Err(_) => {
             match env::home_dir() {
-                Some(path) => match path.to_str() {
-                    Some(e) => File::open(e.to_owned() + "/.boiler/" + filepath + ".boil"),
-                    None => Err(io::Error::new(io::ErrorKind::NotFound, "Your home won't coerce to a string :("))
-                },
-                None => Err(io::Error::new(io::ErrorKind::NotFound, "According to our calculations, you have no home directory.")),
+                Some(path) => {
+                    match path.to_str() {
+                        Some(e) => match File::open(e.to_owned() + "/.boiler/" + filepath + ".boil") {
+                            Ok(f) => Ok(f),
+                            Err(e) => Err(io::Error::new(e.kind(),
+                                       format!("Error with {}: {}", filepath, e))),
+                        },
+                        None => {
+                            Err(io::Error::new(io::ErrorKind::NotFound,
+                                               "Your home won't coerce to a string :("))
+                        }
+                    }
+                }
+                None => {
+                    Err(io::Error::new(io::ErrorKind::NotFound,
+                                       "According to our calculations, you have no home directory."))
+                }
             }
         }
     }
 }
 
 /// Perform line by line boiling. Probably not the best code ever.
-/// Just pass an argument to it.
-fn boil() -> Result<(), io::Error> {
-    let argfile = env::args().nth(1).unwrap();
+fn boil(argfile: String) -> Result<(), io::Error> {
+    println!("Boiling {}", &argfile);
     let mut f = File::open(&argfile)?;
     let mut secret = String::new();
     f.read_to_string(&mut secret)?;
@@ -43,19 +63,44 @@ fn boil() -> Result<(), io::Error> {
     let mut to_write = lns.map(|x| x.to_string() + "\n").collect::<Vec<String>>();
     for ln in to_edit {
         let mut shopper = String::new();
-        shop(to_write[ln].split_whitespace().nth(1).unwrap())?.read_to_string(&mut shopper)?;
+        shop(to_write[ln].split_whitespace().nth(1).unwrap())?
+            .read_to_string(&mut shopper)?;
         to_write[ln] = shopper.as_str().to_owned();
     }
-    let mut nf = File::create(env::args().nth(1).unwrap() + ".boiled")?;
+    let mut nf = File::create(get_boiled_name(&argfile))?;
     for line in to_write {
         nf.write(line.as_bytes())?;
     }
     Ok(())
 }
 
+/// Boils all the files in a list of files.
+fn batch_boil(fl: &mut File) -> io::Result<()> {
+    let mut bufstr = String::new();
+    fl.read_to_string(&mut bufstr)?;
+    for ln in bufstr.lines() {
+        match boil(String::from(ln)) {
+            Ok(()) => println!("Boiling successful!"),
+            Err(e) => println!("Oh no, the recipe didn't turn out alright - {}", e),
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    match boil() {
-        Ok(()) => println!("Boiling successful!"),
-        Err(e) => println!("Oh no, the recipe didn't turn out alright - {}", e),
+    println!("Boiler version 1.1");
+    if env::args().len() > 1 {
+        match boil(env::args().nth(1).unwrap()) {
+            Ok(()) => println!("Boiling successful!"),
+            Err(e) => println!("Oh no, the recipe didn't turn out alright - {}", e),
+        }
+    } else {
+        match File::open("boiler.files.txt") {
+            Ok(mut f) => match batch_boil(&mut f) {
+                Err(e) => println!("Filelist error! {}", e),
+                Ok(()) => println!("Batch completed."),
+            },
+            Err(_) => println!("Nothing to boil."),
+        }
     }
 }
